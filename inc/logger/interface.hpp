60c4afaed0e6412ae4cb95d9f038abe500e56eaf
GanidhuAbey <ganidhu.abey@gmail.com>
2022-01-31 23:24:49 -0800

    updated cmake

--- inc/logger/interface.hpp
+++ inc/logger/interface.hpp
@@ -6,7 +6,7 @@
 	HANDLE ERROR MESSAGES THAT WILL CAUSE TERMINATION
 */
 #define ERR_V_MSG(m_msg) \
-	printf("[ERROR] (in %s at %s on line %u) : %s \n", __FILE__, __PRETTY_FUNCTION__, __LINE__, m_msg);
+	printf("[ERROR] (in %s at %s on line %u) : %s \n", __FILE__, __func__, __LINE__, m_msg);
 
 #define LOG(m_msg) \
-	printf("[LOG] (fn - %s) : %s \n", __PRETTY_FUNCTION__, m_msg);
+	printf("[LOG] (fn - %s) : %s \n", __func__, m_msg);
